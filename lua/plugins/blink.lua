615a4ecb2ecfdb90f6e052e19789f26cb806e53f
p3drohenriquefernandes <28553108+p3drohenriquefernandes@users.noreply.github.com>
2025-08-16 14:46:49 -0300

    updat keymaps and blink.nvim

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -21,26 +21,17 @@ return {
   --- @type blink.cmp.Config
   opts = {
     keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- For an understanding of why the 'default' preset is recommended,
-      -- you will need to read `:help ins-completion`
-      --
-      -- All presets have the following mappings:
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'default',
+      ['<Esc>'] = { 'hide', 'fallback' },
+      ['<C-Space>'] = { 'show' },
+      ['<C-e>'] = { 'hide' },
+      ['<CR>'] = { 'accept', 'fallback' },
+      ['<Tab>'] = { 'select_next' },
+      ['<S-Tab>'] = { 'select_prev' },
+      ['<C-h>'] = { 'snippet_backward' },
+      ['<C-l>'] = { 'snippet_forward' },
+      ['<C-e>'] = { 'hide_documentation' },
+      ['<C-k>'] = { 'show_documentation' },
     },
 
     appearance = {
